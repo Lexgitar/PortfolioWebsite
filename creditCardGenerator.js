40ff1e3f98d9949bcdecbfb33fb5493fb89304f9
Alex Berdea <berdea.alex@yahoo.com>
2022-10-11 21:12:41 +0100

    generate is done move to validate good fing luck

--- creditCardGenerator.js
+++ creditCardGenerator.js
@@ -1,4 +1,7 @@
 let card = [];
+let bott = [];
+let otherBatch = [];
+let anotherBatch = [];
 
 const cardGen=()=>{
   let instances =()=>{return Math.floor(Math.random()*6)+10};
@@ -10,15 +13,48 @@ let numberGen =()=>{return Math.floor(Math.random()*10)};
   }
   return card;
 };
+// COPY 1
+const genCard=()=>{
+  let instances =()=>{return Math.floor(Math.random()*6)+10};
+let motoras =()=>{return Math.floor(Math.random()*10)};
+  for (i=0; i<=instances(); i++ ){
+    bott.push(motoras());
+  }
+  return bott;
+};
+//COPY 2
+const genBatch=()=>{
+  let instances =()=>{return Math.floor(Math.random()*6)+10};
+let motoras =()=>{return Math.floor(Math.random()*10)};
+  for (i=0; i<=instances(); i++ ){
+    otherBatch.push(motoras());
+  }
+  return otherBatch;
+};
+//COPY 3
+const genMore=()=>{
+  let instances =()=>{return Math.floor(Math.random()*6)+10};
+let motoras =()=>{return Math.floor(Math.random()*10)};
+  for (i=0; i<=instances(); i++ ){
+    anotherBatch.push(motoras());
+  }
+  return anotherBatch;
+};
+// rest 
 
-//console.log(cardGen());
-//console.table(cardGen());
+let theGenerator = document.getElementById('genBut');
+let cardOne = document.getElementById('cardNoa');
+let cardTwo = document.getElementById('cardNob');
+let cardThree = document.getElementById('cardNoc');
+let cardFour = document.getElementById('cardNod');
 
-let theGenerator = document.getElementsByClassName("thename");
-let thePlace = document.getElementsByClassName("tester");
-
-const textChange=()=>{
-     thePlace.innerHTML = 'Card-9';
+const textAlter=()=>{
+    cardOne.value = cardGen();
+    cardTwo.value = genCard();
+    cardThree.value = genBatch();
+    cardFour.value = genMore();
+    document.getElementById('genBut').disabled = true;
+    
 };
 
-theGenerator.onclick = textChange;
+theGenerator.onclick = textAlter;
