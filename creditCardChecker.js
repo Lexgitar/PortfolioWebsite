870c349cb2cc8fd716845a301d77486aec5e7829
Alex Berdea <berdea.alex@yahoo.com>
2022-10-12 18:59:26 +0100

    checker done haleluja

--- creditCardChecker.js
+++ creditCardChecker.js
@@ -63,8 +63,7 @@ return total( sumOdd, sumEven );
   //console.log(total(sumOdd,sumEven));
   };
  // test 
- let valik=[9,3,3,6,7,3,4,6,4,7,2,5];
-console.log('this is ' + validateCred(valik));
+ 
 
 let invList = [];
   let vaList = [];
