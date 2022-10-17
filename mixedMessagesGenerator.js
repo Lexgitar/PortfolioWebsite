08929840e4c1a00ebcc3c0307e36a6733c1e61c8
Alex Berdea <berdea.alex@yahoo.com>
2022-10-17 17:29:14 +0100

    mixd msgs done just remember links

--- mixedMessagesGenerator.js
+++ mixedMessagesGenerator.js
@@ -1,3 +1,33 @@
+// dom stuff //
+
+let customBut = document.getElementById('Custom');
+let defaultBut = document.getElementById('Default');
+let genMesBut = document.getElementById("genMess");
+let submitbut = document.getElementById('subBut');
+let ferstInput = document.getElementById("aname");
+let secundInput = document.getElementById("anoname");
+let therdInput = document.getElementById("sup");
+let furthInput = document.getElementById("ansup");
+//--
+//disabled on page load 
+ferstInput.disabled = true;
+secundInput.disabled = true;
+therdInput.disabled = true;
+furthInput.disabled = true;
+submitbut.disabled = true;
+genMesBut.disabled = true;
+
+//custom select 
+const customButEnable = ()=>{
+  genMesBut.innerHTML = "De-generate Message";
+  defaultBut.disabled = true;  
+  ferstInput.disabled = false;
+  secundInput.disabled = false;
+  therdInput.disabled = false;
+  furthInput.disabled = false;
+ // genMesBut.onclick = mesMixerFuncB;
+};
+
 let lex  = ['You ', 'Your ', 'Bob ', 'Joe '];
 let verb   = ['play ', 'are ', 'plays ','is ', 'was '];
 let infin  = 'playing ';
@@ -13,24 +43,24 @@ let genNum3 =()=>{return Math.floor(Math.random()*2)+2};
 let genNum5 =()=>{return Math.floor(Math.random()*2)+3};
 let genNum6 =()=>{return Math.floor(Math.random()*3)+2};
 
-const generator1 = ()=>{
+const generator1 = (hArray)=>{
   
-  let uMan = lex[genNum()]; 
+  let uMan = hArray[genNum()]; 
   let action2 = verb[genNum2()];
   let action4 = verb[genNum5()];
   let action5 = verb[genNum6()];
   let object2 = noun[genNum4()];
   let how = adj[genNum()];
 
-  if (uMan == lex[0] && action2 == verb[1]){
+  if (uMan == hArray[0] && action2 == verb[1]){
 
-    return uMan + action2 + how + prep + object2 + infin;
+    return uMan + action2 + how + prep + infin + object2;
 
-  } else if (uMan == lex[0] && action2 == verb[0]){
+  } else if (uMan == hArray[0] && action2 == verb[0]){
 
     return uMan + action2 + object2 + how;
 
-  }else if ( uMan == lex[1]){
+  }else if ( uMan == hArray[1]){
 
     return uMan + object2 + infin + action4 + how  ;
 
@@ -40,74 +70,63 @@ const generator1 = ()=>{
 
 
 };
-//--
+
 let theBoys  = ['Butcher.', 'Homelander.'];
 
-const generator2 = ()=>{
+
+  
+//theToys.push(therdInput.value, furthInput.value);
+
+const generator2 = (suprM)=>{
 let quote2 = ['. Just like '];
-return quote2 + theBoys[genNum2()];
+return quote2 + suprM[genNum2()];
 };
 
-let pex = ['bubu', 'brbr', 'baba', 'bobo'];
-
-//let msg1 = generator1();
-let msg2 = generator1() + generator2();
-//let msg3 = generator1() + generator2();
-let mixmsg = [msg1, msg2];
+let msg2 = generator1(lex) + generator2(theBoys);
+//let msg3 = generator1(pex) + generator2(theToys);
 
 const mixedmessages = (arrg)=>{
 return arrg;
 };
 
-// dom stuff //
 
-let customBut = document.getElementById('Custom');
-let defaultBut = document.getElementById('Default');
-let genMesBut = document.getElementById('genMes');
-let submitbut = document.getElementById('subBut');
-let ferstInput = document.getElementById("aname");
-let secundInput = document.getElementById("anoname");
-let therdInput = document.getElementById("sup");
-let furthInput = document.getElementById("ansup");
-//disabled on page load 
-genMesBut.disabled = true;
-ferstInput.disabled = true;
-secundInput.disabled = true;
-therdInput.disabled = true;
-furthInput.disabled = true;
-submitbut.disabled = true;
 
-const customButEnable = ()=>{
-  genMesBut.innerHTML = "De-generate Message";
-  defaultBut.disabled = true;  
-  ferstInput.disabled = false;
-  secundInput.disabled = false;
-  therdInput.disabled = false;
-  furthInput.disabled = false;
-  
+
+
+
+const defButDisables =()=>{
+  customBut.disabled = true; 
+  genMesBut.disabled = false;
+  genMesBut.onclick = mesMixerFunc1;
 };
 //
+
 const mesMixerFunc1 = ()=>{
   printSpot.innerHTML = mixedmessages(msg2);
   genMesBut.disabled = true;
 };
 
-const defButDisables =()=>{
-   customBut.disabled = true; 
-   genMesBut.disabled = false;
-   genMesBut.onclick = mesMixerFunc1;
-};
+
+
+
 // new arrays formage
 
  
 const submitting =()=>{
   genMesBut.disabled = false;
   submitbut.disabled = true;
-    
-  
 };
 
+const mesMixerFuncB = ()=>{ 
+  let pex = ['You ', 'Your ', ferstInput.value+" ", secundInput.value+" " ];
+  let theToys = [therdInput.value+".", furthInput.value+"."];
+  let msg3 = generator1(pex) + generator2(theToys);
+  printSpot.innerHTML = mixedmessages(msg3);
+  genMesBut.disabled = true;
+  
+}; 
 
+genMesBut.onclick = mesMixerFuncB;
 customBut.onclick = customButEnable;
 defaultBut.onclick = defButDisables ;
 submitbut.onclick = submitting;
