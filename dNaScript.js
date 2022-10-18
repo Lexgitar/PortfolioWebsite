53c1ab3269bdc7f75135c946ea0ec69fb038a1d6
Alex Berdea <berdea.alex@yahoo.com>
2022-10-18 22:35:39 +0100

    dna is done can't believe it

--- dNaScript.js
+++ dNaScript.js
@@ -9,11 +9,17 @@ let dnaPrint = document.getElementById("dnaOutput");
 
 let ranger = document.getElementById("strAmo");
 
+let firstP = document.getElementById("firstP");
+let secondP = document.getElementById("secondP");
+let thirdP = document.getElementById("thirdP");
+let fourthP = document.getElementById("fourthP");
+let fifthP = document.getElementById("fifthP");
+
 //--- on page-load :
 const onPageLoad=()=>{
-letterTwo.disabled = true;
-letterThree.disabled = true;
-letterFour.disabled = true;
+//letterTwo.disabled = true;
+//letterThree.disabled = true;
+//letterFour.disabled = true;
 passInButton.disabled = true;
 prodButton.disabled = true;
 dnaPrint.disabled = true;
@@ -67,6 +73,7 @@ const mockUpStrand = (lisT) => {
         };
     };
 
+
 const produceDna = ()=>{
     let dnaBases = [letterOne.value, letterTwo.value, letterThree.value, letterFour.value];
    dnaPrint.value = mockUpStrand(dnaBases);
@@ -77,7 +84,54 @@ const produceDna = ()=>{
 };
 prodButton.onclick = produceDna;
 
+const rangerGen =()=>{
+    let strongList = [];
+const generatorX =(howMany)=>{
+    const strongNess2=(strand,arg1, arg2)=>{
+        let scrape=[];
+              for (let i=0; i <strand.length; i++){
+              if((strand[i] === arg1) || (strand[i] === arg2)){
+               scrape.push(strand[i]);
+            }
+         };
+                if (scrape.length>=8){
+                return true;
+         
+                }else {
+                return false;
+         
+            };
+        };
+    
+    let dnaBass = [letterOne.value, letterTwo.value, letterThree.value, letterFour.value]; 
+    let ranList=[];
 
+    const makeP = ()=>{
+           for (let j=0; j<50000; j++){
+           ranList.push( mockUpStrand(dnaBass));
+    }
+           ranList;
+  };
+ 
+   makeP();
 
+  for (let i= 0; i<50000;i++){
+      if (strongNess2(ranList[i], letterTwo.value, letterThree.value)===true && strongList.length < howMany){
+      strongList.push(ranList[i]);
+  }
+};
 
+      return strongList;
+};
 
+generatorX(ranger.value);
+firstP.innerHTML = strongList[0];
+secondP.innerHTML = strongList[1];
+thirdP.innerHTML = strongList[2];
+fourthP.innerHTML = strongList[3];
+fifthP.innerHTML = strongList[4];
+ranger.disabled = true;
+
+};
+
+ranger.addEventListener('change', rangerGen);
