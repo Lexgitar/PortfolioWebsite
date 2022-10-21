6c2efbfd00961706cde19beb9521f0b3fc799692
Alex Berdea <berdea.alex@yahoo.com>
2022-10-21 21:24:19 +0100

    testing 2

--- testing.js
+++ testing.js
@@ -1,12 +1,6 @@
-var deviceIsAndroid = /(android)/i.test(navigator.userAgent);
-
-$(document).ready(function () {
-    if (deviceIsAndroid) {
-        $(document).bind("click", function () {
-            if (document.activeElement.nodeName == 'TEXTAREA' || document.activeElement.nodeName == 'INPUT') {
-                var textBox = document.activeElement.id;
-                document.getElementById(textBox).scrollIntoView();
-            }
-        });
-    }
-});
+const getPixels = () => {
+    const screenHeight = window.innerHeight;
+    return (screenHeight / 100) * percentage;
+  };
+  
+  elem.style.minHeight = `${getPixels}px`;
